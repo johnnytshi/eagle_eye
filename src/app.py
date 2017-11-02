b10a1c2e028509e06f820a72ca356f9b464f2539
joshi <johnny.shi@visiercorp.com>
2017-11-01 22:26:44 -0700

    Parameterize min threshold

--- src/app.py
+++ src/app.py
@@ -20,13 +20,12 @@ PATH_TO_CKPT = '/model_zoo/' + MODEL_NAME + '/frozen_inference_graph.pb'
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('/models/research/object_detection', 'data', 'mscoco_label_map.pbtxt')
 NUM_CLASSES = 90
-MIN_SCORE_THRESH = 0.6
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-def detect_objects(output_q, image_np, sess, image_tensor, boxes, scores, classes, num_detections):
+def detect_objects(output_q, image_np, sess, image_tensor, boxes, scores, classes, num_detections, min_score_thresh):
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
 
@@ -37,7 +36,7 @@ def detect_objects(output_q, image_np, sess, image_tensor, boxes, scores, classe
 
     scores_squeezed = np.squeeze(scores)
     for val in scores_squeezed:
-        if val > MIN_SCORE_THRESH:
+        if val >= min_score_thresh:
             break
     else:
         print("NO object detected")
@@ -57,7 +56,7 @@ def detect_objects(output_q, image_np, sess, image_tensor, boxes, scores, classe
         line_thickness=8)
     output_q.put(image_np)
 
-def input_worker(input_q, output_q):
+def input_worker(input_q, output_q, min_score_thresh):
     # Load a (frozen) Tensorflow model into memory.
     detection_graph = tf.Graph()
     with detection_graph.as_default():
@@ -83,7 +82,7 @@ def input_worker(input_q, output_q):
     while True:
         frame = input_q.get()
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        detect_objects(output_q, frame_rgb, sess, image_tensor, boxes, scores, classes, num_detections)
+        detect_objects(output_q, frame_rgb, sess, image_tensor, boxes, scores, classes, num_detections, min_score_thresh)
 
     sess.close()
 
@@ -101,11 +100,13 @@ def output_worker(output_q, slack_token, slack_channel):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-src', '--source', dest='video_source', type=str,
-                        default=0, help='rtsp://admin:*@192.*.*.*:554/h264/ch1/main/av_stream')
+                        help='rtsp://admin:*@192.*.*.*:554/h264/ch1/main/av_stream')
     parser.add_argument('-token', '--slack_token', dest='slack_token', type=str,
-                        default=480, help='Slack Web API Token')
+                        help='Slack Web API Token')
     parser.add_argument('-channel', '--slack_channel', dest='slack_channel', type=str,
-                        default=480, help='Slack Web API Token')
+                        help='Slack Web API Token')
+    parser.add_argument('-min_score_thresh', '--min_score_thresh', dest='min_score_thresh', type=float,
+                        default=0.5, help='percentage as min confidence score')
     args = parser.parse_args()
     print(args)
 
@@ -114,7 +115,7 @@ if __name__ == '__main__':
 
     input_q = Queue(24)
     output_q = Queue(24)
-    input_pool = Pool(2, input_worker, (input_q, output_q))
+    input_pool = Pool(2, input_worker, (input_q, output_q, args.min_score_thresh))
     output_pool = Pool(1, output_worker, (output_q, args.slack_token, args.slack_channel))
 
     video_capture = cv2.VideoCapture(args.video_source)
@@ -123,7 +124,7 @@ if __name__ == '__main__':
 
         if ret:
             input_q.put(frame)
-        time.sleep(.3)
+        time.sleep(.2)
 
         if 0xFF == ord('q'):
             break
