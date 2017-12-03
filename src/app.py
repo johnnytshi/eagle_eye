02d9428e2cd8bf4f3d7fb1547a1665ad913827a6
joshi <johnny.shi@visiercorp.com>
2017-12-03 13:40:34 -0800

    push tf session creation to each worker

--- src/app.py
+++ src/app.py
@@ -25,9 +25,20 @@ label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-def detect_objects(image_np, sess, image_tensor, boxes, scores, classes, num_detections, min_score_thresh):
+def detect_objects(image_np, sess, min_score_thresh):
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
+
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    # Each box represents a part of the image where a particular object was detected.
+
+    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
     # Actual detection.
     (boxes, scores, classes, num_detections) = sess.run(
@@ -55,15 +66,29 @@ def detect_objects(image_np, sess, image_tensor, boxes, scores, classes, num_det
     print('D', end='')
     return True
 
-def detector_worker(input_q, output_q, sess, image_tensor, boxes, scores, classes, num_detections, min_score_thresh):
+def detector_worker(input_q, output_q, min_score_thresh):
+    print('tensorflow init')
+    # init tensorflow
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+        sess = tf.Session(graph=detection_graph)
+    print('tensorflow init done')
     while True:
         frame = input_q.get()
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        object_detected = detect_objects(frame_rgb, sess, image_tensor, boxes, scores, classes, num_detections, min_score_thresh)
+        object_detected = detect_objects(frame_rgb, sess, min_score_thresh)
         if object_detected:
             input_q.put(frame_rgb)
 
-def notifier_worker(output_q, sc):
+def notifier_worker(output_q, slack_token):
+    # init Slack
+    sc = SlackClient(args.slack_token)
     while True:
         frame_rgb = output_q.get()
         print('slack')
@@ -85,38 +110,13 @@ if __name__ == '__main__':
                         default=0.5, help='percentage as min confidence score')
     args = parser.parse_args()
 
-    # init tensorflow
-    detection_graph = tf.Graph()
-    with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-            serialized_graph = fid.read()
-            od_graph_def.ParseFromString(serialized_graph)
-            tf.import_graph_def(od_graph_def, name='')
-
-        sess = tf.Session(graph=detection_graph)
-    print('tensorflow init')
-    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-    # Each box represents a part of the image where a particular object was detected.
-
-    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-
-    # Each score represent how level of confidence for each of the objects.
-    # Score is shown on the result image, together with the class label.
-    scores = detection_graph.get_tensor_by_name('detection_scores:0')
-    classes = detection_graph.get_tensor_by_name('detection_classes:0')
-    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-    print('tensorflow init done')
-    # init OpenCV
-    video_capture = cv2.VideoCapture(args.video_source)
-
-    # init Slack
-    sc = SlackClient(args.slack_token)
-
     input_q = Queue(maxsize=30)
     output_q = Queue(maxsize=5)
-    detector_pool = Pool(2, detector_worker, (input_q, output_q, sess, image_tensor, boxes, scores, classes, num_detections, args.min_score_thresh))
-    notifier_pool = Pool(1, notifier_worker, (output_q, sc))
+    detector_pool = Pool(2, detector_worker, (input_q, output_q, args.min_score_thresh))
+    notifier_pool = Pool(1, notifier_worker, (output_q, args.slack_token))
+
+    # init OpenCV
+    video_capture = cv2.VideoCapture(args.video_source)
 
     fps = FPS().start()
 
