7646654c94bf24e98e0039e9d3fccd4788452257
johnnytshi <johnny.t.shi@gmail.com>
2017-12-04 22:16:46 -0800

    Add sleep time as Param

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ CMD python3 app.py \
     --source=$SOURCE \
     --slack_token=$SLACK_TOKEN \
     --slack_channel=$SLACK_CHANNEL \
-    --min_score_thresh=$MIN_SCORE_THRESH
+    --min_score_thresh=$MIN_SCORE_THRESH \
+    --sleep_time=$SLEEP_TIME
--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from multiprocessing import Queue, Pool
 import time
 from imutils.video import FPS
-
+import logging
 sys.path.append('/models/research/object_detection/')
 from utils import label_map_util
 from utils import visualization_utils as vis_util
@@ -26,6 +26,7 @@ categories = label_map_util.convert_label_map_to_categories(label_map, max_num_c
 category_index = label_map_util.create_category_index(categories)
 
 def detect_objects(image_np, sess, detection_graph, min_score_thresh):
+    logging.debug('running detection')
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
 
@@ -46,8 +47,11 @@ def detect_objects(image_np, sess, detection_graph, min_score_thresh):
         feed_dict={image_tensor: image_np_expanded})
 
     scores_squeezed = np.squeeze(scores)
-    for val in scores_squeezed:
-        if val >= min_score_thresh:
+    classes_squeezed = np.squeeze(classes).astype(np.int32)
+    #logging.debug(scores_squeezed)
+    #logging.debug(classes_squeezed)
+    for i in range(len(scores_squeezed)):
+        if scores_squeezed[i] >= min_score_thresh and classes_squeezed[i] == 1:
             break
     else:
         return False
@@ -56,7 +60,7 @@ def detect_objects(image_np, sess, detection_graph, min_score_thresh):
     vis_util.visualize_boxes_and_labels_on_image_array(
         image_np,
         np.squeeze(boxes),
-        np.squeeze(classes).astype(np.int32),
+        classes_squeezed,
         scores_squeezed,
         category_index,
         use_normalized_coordinates=True,
@@ -79,25 +83,29 @@ def detector_worker(input_q, output_q, min_score_thresh):
         sess = tf.Session(graph=detection_graph)
     print('tensorflow init done')
     while True:
+        logging.debug('getting pic')
         frame = input_q.get()
+        logging.debug('got pic')
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         object_detected = detect_objects(frame_rgb, sess, detection_graph, min_score_thresh)
         if object_detected:
-            input_q.put(frame_rgb)
+            output_q.put(frame_rgb)
 
 def notifier_worker(output_q, slack_token):
     # init Slack
     sc = SlackClient(args.slack_token)
     while True:
         frame_rgb = output_q.get()
-        print('slack')
+        logging.debug('slack')
         cv2.imwrite("image.jpg", frame_rgb)
         in_file = open("image.jpg", "rb") # opening for [r]eading as [b]inary
-        data = in_file.read()
-        res = sc.api_call("files.upload", filename="image.jpg", channels="#eagle_eye", file=data)
-        print(res)
+        #data = in_file.read()
+        res = sc.api_call("files.upload", filename="image.jpg", channels="#eagle_eye", file=in_file)
+        logging.debug(res)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-src', '--source', dest='video_source', type=str,
                         help='rtsp://admin:*@192.*.*.*:554/h264/ch1/main/av_stream')
@@ -107,10 +115,12 @@ if __name__ == '__main__':
                         help='Slack Web API Token')
     parser.add_argument('-min_score_thresh', '--min_score_thresh', dest='min_score_thresh', type=float,
                         default=0.5, help='percentage as min confidence score')
+    parser.add_argument('-sleep_time', '--sleep_time', dest='sleep_time', type=float,
+                        default=0.3, help='sleep time between each frame') 
     args = parser.parse_args()
 
-    input_q = Queue(maxsize=30)
-    output_q = Queue(maxsize=5)
+    input_q = Queue(maxsize=50)
+    output_q = Queue(maxsize=50)
     detector_pool = Pool(2, detector_worker, (input_q, output_q, args.min_score_thresh))
     notifier_pool = Pool(1, notifier_worker, (output_q, args.slack_token))
 
@@ -122,9 +132,10 @@ if __name__ == '__main__':
     while(1):
         ret, frame = video_capture.read()
         if ret:
-            print(input_q.qsize())
+            logging.debug(input_q.qsize())
             input_q.put(frame)
-            time.sleep(.25)
+            logging.debug('putting pic')
+            time.sleep(args.sleep_time)
         else:
             print('video open')
             video_capture.open(args.video_source)
